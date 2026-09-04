f0808d87629f105b19e41574955c605b52a7501a
Dishant patel <dishantp369@gmail.com>
2026-09-04 15:17:24 +0530

    Fix production CORS configuration

--- Server/index.js
+++ Server/index.js
@@ -12,10 +12,23 @@ const app = express();
 
 connectDB();
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://lecture-scheduler-one.vercel.app",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
