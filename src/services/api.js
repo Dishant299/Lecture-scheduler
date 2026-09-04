7e8d2bdb6a825bed58260c39d8b6aa4db3ce1a9e
Dishant patel <dishantp369@gmail.com>
2026-09-04 15:01:13 +0530

    added backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: "https://lecture-scheduler-4s8l.onrender.com/api/v1",
   withCredentials: true,
 });
 
